94a0b41d6ca354dabf0429f0d08a21c395c476a0
Jayden Windle <jaydenwindle@gmail.com>
2016-06-18 14:59:50 -0400

    debugging

--- index.js
+++ index.js
@@ -27,42 +27,7 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook', function(req, res) {
     console.log(req.body);
-    var data = req.body;
-
-    // Make sure this is a page subscription
-    if (data.object == 'page') {
-        // Iterate over each entry
-        // There may be multiple if batched
-        data.entry.forEach(function(pageEntry) {
-            var pageID = pageEntry.id;
-            var timeOfEvent = pageEntry.time;
-
-            // Iterate over each messaging event
-            pageEntry.messaging.forEach(function(messagingEvent) {
-            if (messagingEvent.optin) {
-                console.log("Recieved Auth: " + messagingEvent)
-                // receivedAuthentication(messagingEvent);
-            } else if (messagingEvent.message) {
-                console.log("Recieved Message: " + messagingEvent)
-                // receivedMessage(messagingEvent);
-            } else if (messagingEvent.delivery) {
-                console.log("Recieved Delivery: " + messagingEvent)
-                // receivedDeliveryConfirmation(messagingEvent);
-            } else if (messagingEvent.postback) {
-                console.log("Recieved Postback: " + messagingEvent)
-                // receivedPostback(messagingEvent);
-            } else {
-                console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-            }
-            });
-        });
-
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know you've 
-        // successfully received the callback. Otherwise, the request will time out.
-        res.sendStatus(200); 
-    }
+    res.send(200);
 });
 
 app.listen(app.get('port'), function() {
