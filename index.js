cea7346eda64dfa8ac0cef68a156fd155ff9658e
Jayden Windle <jaydenwindle@gmail.com>
2016-06-18 13:58:06 -0400

    --squash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var config = req("config.json");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,14 +8,25 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+});
+
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === config.webhook_tokent) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);          
+    }  
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
