3f5f9409175418c52d95a1a5fd422152572577b6
Jayden Windle <jaydenwindle@gmail.com>
2016-06-18 15:49:36 -0400

    initail respond api

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post('/webhook', function(req, res) {
             pageEntry.messaging.forEach(function(messagingEvent) {
             if (messagingEvent.optin) {
                 console.log("Recieved Auth: " + JSON.stringify(messagingEvent));
+                message.send(messagingEvent.sender.id, "Hello there!");
                 //receivedAuthentication(messagingEvent);
             } else if (messagingEvent.message) {
                 console.log("Recieved Message: " + JSON.stringify(messagingEvent));
