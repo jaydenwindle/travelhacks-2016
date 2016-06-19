222031d472ca86b00ab6eb30de793f5e76b5143d
Jayden Windle <jaydenwindle@gmail.com>
2016-06-18 20:12:05 -0400

    Fixed getProfile

--- index.js
+++ index.js
@@ -41,13 +41,51 @@ function getProfile(id, callback) {
     request.get({
         uri: 'https://graph.facebook.com/v2.6/' + id,
         qs: {
-            fields: 'first_name, last_name, profile_pic, locale, timezone, gender',
+            fields: 'first_name, last_name, locale, timezone, gender',
             access_token: process.env.page_token,
         }
     }, function (err, resp, profile) {
         console.log("getProfile: " + profile);
         callback(JSON.parse(profile));
     });
+}
+
+function send(recipientId, messageText) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: messageText
+		}
+	};
+
+    console.log('Calling send api');
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: process.env.page_token },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s", 
+            messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    }, function (err, resp, body) {
+        console.log(body);
+    });  
 }
 
 // Wit.ai Actions
--- messaging.js
+++ messaging.js
@@ -1,43 +1,7 @@
 var request = require('request');
 var config = require('./config.json');
 
-function send(recipientId, messageText) {
-	var messageData = {
-		recipient: {
-			id: recipientId
-		},
-		message: {
-			text: messageText
-		}
-	};
 
-    console.log('Calling send api');
-	callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-    request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: process.env.page_token },
-        method: 'POST',
-        json: messageData
-
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var recipientId = body.recipient_id;
-            var messageId = body.message_id;
-
-            console.log("Successfully sent generic message with id %s to recipient %s", 
-            messageId, recipientId);
-        } else {
-            console.error("Unable to send message.");
-            console.error(response);
-            console.error(error);
-        }
-    }, function (err, resp, body) {
-        console.log(body);
-    });  
-}
 
 
 module.exports = {
