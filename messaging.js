ccccf193aeb9d5fec3ae2a55fae988b498c98903
Jayden Windle <jaydenwindle@gmail.com>
2016-06-18 20:02:36 -0400

    Fixed getProfile

--- messaging.js
+++ messaging.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var config = require('./config.json');
 
-send = function (recipientId, messageText) {
+function send(recipientId, messageText) {
 	var messageData = {
 		recipient: {
 			id: recipientId
